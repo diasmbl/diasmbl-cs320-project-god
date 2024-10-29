c433552fa409145ec2ed87d2f1c28ccf87e1cc5c
diasmbl <155988823+diasmbl@users.noreply.github.com>
2024-10-28 22:55:22 -0600

    6.3 Develop the Database

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Page2 from './Page2';
-import Page3 from './Page3';  // Import Page3 for Starter Roulette
-import Navbar from './Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css'; // Importing global CSS
+import App from './App'; // Home page component
+import Page2 from './Page2'; // Region Roulette component
+import Page3 from './Page3'; // Starter Roulette component
+import Navbar from './Navbar'; // Navbar component for navigation
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // React Router for navigation
 
+// Creating the root for rendering the application
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Rendering the main application with React Router
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Navbar /> {/* Navbar with navigation links */}
+      <Navbar /> {/* Navbar with links to different pages */}
       <Routes>
-        <Route path="/" element={<App />} /> {/* Home page */}
-        <Route path="/page2" element={<Page2 />} /> {/* Region Roulette */}
-        <Route path="/page3" element={<Page3 />} /> {/* Starter Roulette */}
+        <Route path="/" element={<App />} /> {/* Home route */}
+        <Route path="/page2" element={<Page2 />} /> {/* Region Roulette route */}
+        <Route path="/page3" element={<Page3 />} /> {/* Starter Roulette route */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
